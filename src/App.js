61b5429bbe9ee5bae6ae659dc825ed33ce3b4a59
Dzmitry Sashcheka <sheensheella@gmail.com>
2024-02-22 14:20:36 +0300

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Header } from './components/Header'
+
 function App() {
-  return <div className="App">Start Project</div>
+  return (
+    <>
+      <Header />
+    </>
+  )
 }
 
 export default App
